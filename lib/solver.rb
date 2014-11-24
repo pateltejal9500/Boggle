f3cc56b6dbd9585d76a7a320f69b03d5bf0ffa7d
pateltejal9500 <pateltejal9500@gmail.com>
2014-11-24 01:01:40 -0500

    notes

--- lib/solver.rb
+++ lib/solver.rb
@@ -7,6 +7,7 @@ class Board
   def initialize(board, trie)
     @trie = trie
     @words = []
+    #all the directions of movement
     @possible_directions = [ [-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1] ]
     @board = board
     @index_start1 = 0
@@ -15,6 +16,7 @@ class Board
     @used = Array.new(4) {[0] * 4}
   end
 
+#this is for every letter on the board when it is the first letter
   def boardSolver1st()
     @used[@index_start1][@index_start2] = 1  
     prefix = @board[@index_start1][@index_start2]
@@ -22,8 +24,10 @@ class Board
     move()
   end
 
+#this will follow the prefix until it doesn't match anything in the trie
   def boardSolver2(index1, index2, prefix1)
     @possible_directions.each do |x,y|
+      #making sure that the movement doesn't take you off the board
       if (0..3) === (x + index1) && (0..3) === (y + index2) 
         prefix = prefix1 + @board[x + index1][y + index2]
         if find_prefix(prefix) == true
@@ -45,11 +49,13 @@ class Board
       @words << prefixArray[i]
     end
     @prefix = {}
+    #if there were prefixes that can become words, it will continue going
     boardSolver2(index[0].to_i, index[1].to_i, prefixArray[i])
     i += 1
   end
 end
 
+#checking if the word is in the dictionary trie
 def is_word(word)
   if @trie.has_key?(word) == true && word.length > 2 && @words.include?(word) == false
     return true
@@ -58,6 +64,7 @@ def is_word(word)
   end
 end
 
+#checking if the prefix is in any word in the dictionary trie
 def find_prefix(prefix)
   if @trie.children(prefix).length > 0
     return true
@@ -70,6 +77,8 @@ def words
   return @words
 end
 
+#all the different letters on the board when they have to be the starting point 
+#after they have all been used, the game is over and the words are returned
 def move() 
   if @used[0][1] == 0  
     @index_start1 = 0
--- server.rb
+++ server.rb
@@ -13,14 +13,16 @@ row3 = gets.chomp
 puts "Please type the four letters in the fourth row with no spaces"
 row4 = gets.chomp
 
+#this is going make the letters into an array
 row1 = row1.split("")
 row2 = row2.split("")
 row3 = row3.split("")
 row4 = row4.split("")
 
-
+#this will make the board a 2D array
 board = [row1, row2, row3, row4]
  
+ #reading the dictionary from the file, turning into an array, and adding each work to the trie created
 dictionary = File.read('./words.txt')
   dictionary_words = dictionary.split(" ")
   trie = Trie.new
